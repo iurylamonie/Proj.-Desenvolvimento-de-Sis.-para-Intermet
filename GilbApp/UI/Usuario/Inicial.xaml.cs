9a2bbb572466eefe44bd0bcad3e87b2b55a10178
Iury Lamonie <iury.lamonie@hotmail.com>
2017-01-30 14:42:28 -0300

    Criação das Telas

---
+++ GilbApp/UI/Usuario/Inicial.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace UI.Usuario
+{
+    public partial class Inicial : PhoneApplicationPage
+    {
+        public Inicial()
+        {
+            InitializeComponent();
+            TextBlockNomeLogado.Text = (App.Current as App).NomeUsuarioLogado;
+        }
+    }
+}
