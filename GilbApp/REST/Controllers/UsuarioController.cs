933c9bfda05b8f8a97ff7331353566727fd5947b
Iury Lamonie <iury.lamonie@hotmail.com>
2017-01-30 17:41:10 -0300

    Criando REST

---
+++ GilbApp/REST/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST.Controllers
+{
+    [RoutePrefix("api/Usuario")]
+    public class UsuarioController : ApiController
+    {
+        [AcceptVerbs("POST")]
+        [Route("Inserir")]
+        public void Inserir([FromBody] string conteudo)
+        {
+            Models.Usuario usuario = JsonConvert.DeserializeObject<Models.Usuario>(conteudo);
+            Models.WhatsappDataContext dc = new Models.WhatsappDataContext();
+            dc.Usuarios.InsertOnSubmit(usuario);
+            dc.SubmitChanges();
+        }
+    }
+}
